2d6c93ecb285a59486c5abf81f8e441d043a76a3
Aditya Saini <2k22.cscys.2212326@gmail.com>
2025-08-04 13:16:15 +0530

    Update CodeinJava.java

--- 12- 3 Sum/Code/CodeinJava.java
+++ 12- 3 Sum/Code/CodeinJava.java
@@ -1,1 +1,58 @@
+//Brute Force
+//Time complexity: O(n^3)
+//Space complexity:O(m)
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> res = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> tmp = Arrays.asList(nums[i], nums[j], nums[k]);
+                        res.add(tmp);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(res);
+    }
+}
 
+
+//Two Pointers
+//Time complexity: O(n^2)
+//Space complexity:O(1) or O(n) extra space depending on the sorting algorithm.
+//O(m) space for the output list.
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return new ArrayList<>();
+        }
+        Arrays.sort(nums);
+
+        Set<List<Integer>> result = new HashSet<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+
+        }
+        return new ArrayList<>(result);
+
+    }
+}
